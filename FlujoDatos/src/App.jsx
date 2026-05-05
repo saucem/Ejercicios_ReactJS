dbb93a5e76573e9c1bfb03967badeb79bcebf0f2
Mauro Saucedo <saucem.dev@gmail.com>
2026-05-04 21:05:59 -0300

    Preparación de estructura para el ejercicio teórico de la clase 6: formularios y subida de imágenes

--- FlujoDatos/src/App.jsx
+++ FlujoDatos/src/App.jsx
@@ -1,6 +1,7 @@
 import Layout from "./components/Layout/Layout.jsx"
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer.jsx";
 import { Directory } from "./components/Directory/Directory.jsx";
+import { ProductForm } from "./components/ProductForm/ProductForm.jsx";
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <>
           <h3>Productos</h3>
           <ItemListContainer Mensaje="Productos Destacados" />
+          
           <Directory Mensaje="Nuestro Equipo" />
         </>
       }
