977b89195cd33e27b2594752ffb43c9990775b1d
Mauro Saucedo <saucem@gmail.com>
2026-06-04 02:52:48 -0300

    Se quitó la estrucura de navegación inicial, reemplazada por componentes de RB

--- Proyecto/src/components/Layout/Navigation/Navigation.jsx
+++ Proyecto/src/components/Layout/Navigation/Navigation.jsx
@@ -40,33 +40,6 @@ export function Navigation() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      
-      {/*
-      <nav className={styles.nav}>
-        <div className={styles.navbrand}>
-          <span>
-            <img src="../images/icons/goodwork_logo_black.svg" alt="Logo WoodWork" width={"48"} />
-            GoodWork
-          </span>
-        </div>
-        <div className={styles.navbar}>
-          <ul>
-            <li><Link className={styles.navlink} to={"/"}>Inicio</Link></li>
-            <li><Link className={styles.navlink} to={"/productos"}>Productos</Link></li>
-            <li><Link className={styles.navlink} to={"/destacados"}>Destacados</Link></li>
-            <li><Link className={styles.navlink} to={"/"}>Contacto</Link></li>
-          </ul>
-        </div>
-        <div className={styles.navtrail}>
-          <div className={styles.navbar}>
-            <ul>
-              <li><Link className={styles.navlink} to={"/altaproducto"}>Stock</Link></li>
-              <li><Link className={styles.navlinkImage} to={"/carrito"}><img src="../images/icons/shopping_cart_32dp.svg" alt="Imagen de un carrito de compras" width={"32"} /></Link></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      */}
     </>
   )
 } 
