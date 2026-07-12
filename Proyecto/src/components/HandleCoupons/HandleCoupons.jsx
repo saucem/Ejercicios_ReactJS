9e139e070f0943f2ccb553c6c77968c4bfecffd9
Mauro Saucedo <saucem@gmail.com>
2026-07-12 03:17:27 -0300

    Proyecto. Agregado de React-icons. Funcionlidad y estilos para el manejo de cuponer

--- Proyecto/src/components/HandleCoupons/HandleCoupons.jsx
+++ Proyecto/src/components/HandleCoupons/HandleCoupons.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
+import { toast } from "react-toastify";
+import { BsTrash, BsPencil } from "react-icons/bs";
 import { db } from "../../firebase/config";
 import {
   collection,
@@ -12,6 +15,7 @@ function HandleCoupons() {
   const [coupons, setCoupons] = useState([]);
 
   const [code, setCode] = useState("");
+  const [expiration, setExpiration] = useState("");
   const [discount, setDiscount] = useState("");
 
   const [showForm, setShowForm] = useState(false);
@@ -27,7 +31,7 @@ function HandleCoupons() {
       setCoupons(couponsList);
     } catch (error) {
       console.error("Error al obtener los cupones:", error);
-      alert("ocurrió un error al cargar los cupones.");
+      toast.error("Ocurrió un error al cargar los cupones.");
     }
   };
 
@@ -35,33 +39,34 @@ function HandleCoupons() {
   const createCoupon = async (e) => {
     e.preventDefault();
 
-    setShowForm(!showForm);
-
     if (!code || !discount) {
-      alert("Complete todos los campos");
+      toast.error("Complete todos los campos");
       return;
     }
 
     const percent = Number(discount);
 
     if (percent < 1 || percent > 100) {
-      alert("El descuento debe estar entre 1 y 100.");
+      toast.info("El descuento debe estar entre 1 y 100.");
       return;
     }
 
     try {
       await addDoc(collection(db, "coupons"), {
         code,
+        expiration,
         discount: Number(discount),
       });
 
       setCode("");
       setDiscount("");
+      setShowForm(false);
+      toast.success("Cupón creado!", { autoClose: 2000 });
 
       await getCoupons();
     } catch (error) {
       console.error(error);
-      alert("Error al crear el cupón.");
+      toast.error("Error al crear el cupón.");
     }
   };
 
@@ -70,12 +75,12 @@ function HandleCoupons() {
     try {
       await deleteDoc(doc(db, "coupons", id));
 
-      alert("Cupón eliminado correctamente");
+      toast.success("Cupón eliminado correctamente");
 
       await getCoupons();
     } catch (error) {
       console.error(error);
-      alert("Error al eliminar el cupón.");
+      toast.error("Error al eliminar el cupón.");
     }
   };
 
@@ -84,71 +89,135 @@ function HandleCoupons() {
   }, []);
 
   return (
-    <div
-      style={{ color: "white", minHeight: "90vh" }}
-      className="container d-flex flex-column"
-    >
+    <div className="container d-flex flex-column above-the-fold text-light">
       <div>
-        <h2>Administración de Cupones</h2>
+        <h2 className="mb-3">Administración de Cupones</h2>
       </div>
 
       {showForm && (
         <div className="container">
-          <form action="">
-            <label htmlFor="">Código</label>
-            <input type="text" />
-            <label htmlFor="">Valido hasta</label>
-            <input type="date" />
-            <label htmlFor="">Descuento %</label>
-            <input type="date" />
-          </form>
+          <div className="row justify-content-center">
+            <div className="col-md-6 col-lg-4">
+              <form className="border border-1 border-secondary rounded-2 p-3">
+                <div className="mb-3">
+                  <label htmlFor="code" className="form-label">
+                    Código
+                  </label>
+                  <input
+                    type="text"
+                    name="code"
+                    className="form-control"
+                    onChange={(e) => setCode(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="expiration" className="form-label">
+                    Válido hasta
+                  </label>
+                  <input
+                    type="date"
+                    name="expiration"
+                    className="form-control"
+                    onChange={(e) => setExpiration(e.target.value)}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="discount" className="form-label">
+                    Descuento %
+                  </label>
+                  <input
+                    type="number"
+                    name="discount"
+                    className="form-control"
+                    onChange={(e) => setDiscount(e.target.value)}
+                  />
+                </div>
+                <div className="d-flex justify-content-between gap-3">
+                  <button
+                    className="btn btn-success col-auto"
+                    onClick={createCoupon}
+                    type="submit"
+                  >
+                    Guardar cambios
+                  </button>
+                  <button
+                    className="btn btn-secondary col"
+                    type="button"
+                    onClick={() => setShowForm(false)}
+                  >
+                    Cancelar
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       )}
-      {coupons.length ? (
-        <div className="row">
-          <h3>Listado de Cupones</h3>
-          <table className="table table-hover">
+      {coupons.length > 0 && (
+        <div className="container mt-4">
+          <Table hover >
             <thead>
-              <th>Código del cupón</th>
-              <th>Válido hasta</th>
-              <th>Descuento</th>
-              <th>Acciones</th>
+              <tr>
+                <th>Código del cupón</th>
+                <th>Válido hasta</th>
+                <th className="text-center">Descuento</th>
+                <th className="text-center">Acciones</th>
+              </tr>
             </thead>
             <tbody>
               {coupons.map((coupon) => (
                 <tr key={coupon.id}>
                   <td>{coupon.code}</td>
                   <td>{coupon.expiration}</td>
-                  <td>{coupon.discount}</td>
-                  <td>
-                    <button onClick={() => deleteCoupon(coupon.id)}>
-                      Eliminar
+                  <td className="text-center">{coupon.discount}%</td>
+                  <td className="text-center">
+                    <button
+                      onClick={() => deleteCoupon(coupon.id)}
+                      className="action-link me-3"
+                    >
+                      <BsTrash className="me-2" />Eliminar
                     </button>
-                    <button onClick={() => updateCoupon(coupon.id)}>
-                      Editar
+                    {/*
+                    <button
+                      onClick={() => updateCoupon(coupon.id)}
+                      className="action-link"
+                    >
+                      <BsPencil className="me-2" />Editar
                     </button>
+                    */}
                   </td>
                 </tr>
               ))}
             </tbody>
-          </table>
-        </div>
-      ) : (
-        <div className="row text-center">
-          <h3>No hay cupones activos</h3>
-          <p>Hacé clic en "Crear cupón" para crear uno nuevo.</p>
+          </Table>
         </div>
       )}
-      <div className="container">
-        <div className="row justify-content-center my-4">
-          <button
-            onClick={() => createCoupon()}
-            className="col-md-4 col-lg-2 btn btn-success"
-          >
-            {editMode ? "Guardar cambios" : "Crear cupón"}
+
+      {!showForm && coupons.length == 0 && (
+        <div className="container">
+          <div className="row text-center justify-content-center my-4">
+            <h3>No hay cupones activos</h3>
+            <p>Hacé clic en "Crear cupón" para crear uno nuevo.</p>
+          </div>
+        </div>
+      )}
+
+      {showForm ? (
+        <div className="container">
+          <button className="col-md-4 col-lg-2 btn btn-warning mt-4" disabled>
+            Crear cupón
           </button>
         </div>
-      </div>
+      ) : (
+        <div className="container">
+          <button
+            className="col-md-4 col-lg-2 btn btn-warning mt-4"
+            onClick={() => setShowForm(true)}
+          >
+            Crear cupón
+          </button>
+        </div>
+      )}
     </div>
   );
 }
